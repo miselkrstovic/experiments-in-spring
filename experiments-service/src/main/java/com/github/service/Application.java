fbbdea955483734af2f714eac01fc588eeab73fe
Mišel Krstović <miselkrstovic@users.noreply.github.com>
2020-09-15 11:40:47 +0200

    Remove unnecessary annotations

--- experiments-service/src/main/java/com/github/service/Application.java
+++ experiments-service/src/main/java/com/github/service/Application.java
@@ -2,12 +2,10 @@ package com.github.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-@ComponentScan(basePackages = "com.github.service")
 public class Application {
 
 	public static void main(String[] args) {
